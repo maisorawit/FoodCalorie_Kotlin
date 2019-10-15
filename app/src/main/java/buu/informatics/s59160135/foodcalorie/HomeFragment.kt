fc287dec38f556e0489eb469f9fe63d33afcf293
maisorawit <maisorawit.st@gmail.com>
2019-10-16 01:48:36 +0700

    Add About

--- app/src/main/java/buu/informatics/s59160135/foodcalorie/HomeFragment.kt
+++ app/src/main/java/buu/informatics/s59160135/foodcalorie/HomeFragment.kt
@@ -2,12 +2,12 @@ package buu.informatics.s59160135.foodcalorie
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import buu.informatics.s59160135.foodcalorie.databinding.FragmentFoodMenuBinding
 import buu.informatics.s59160135.foodcalorie.databinding.FragmentHomeBinding
 
@@ -20,19 +20,30 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,
-            R.layout.fragment_home, container, false)
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(
+            inflater,
+            R.layout.fragment_home, container, false
+        )
 
-        binding.buttonFood.setOnClickListener{view ->
+        binding.buttonFood.setOnClickListener { view ->
             view.findNavController().navigate(R.id.action_homeFragment_to_foodMenuFragment)
         }
 
-        binding.buttonDrink.setOnClickListener{view ->
+        binding.buttonDrink.setOnClickListener { view ->
             view.findNavController().navigate(R.id.action_homeFragment_to_drinkMenuFragment)
         }
-
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            view!!.findNavController()) || super.onOptionsItemSelected(item)
+    }
 
 }
