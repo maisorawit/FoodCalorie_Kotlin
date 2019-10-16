91a51f1f866fcff152403f0d07559e1d1375e53d
maisorawit <maisorawit.st@gmail.com>
2019-10-16 08:08:40 +0700

    Finish HomeV1

--- app/src/main/java/buu/informatics/s59160135/foodcalorie/DrinkMenuFragment.kt
+++ app/src/main/java/buu/informatics/s59160135/foodcalorie/DrinkMenuFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import buu.informatics.s59160135.foodcalorie.databinding.FragmentDrinkMenuBinding
 import buu.informatics.s59160135.foodcalorie.databinding.FragmentFoodMenuBinding
 
@@ -18,6 +19,27 @@ class DrinkMenuFragment : Fragment() {
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentDrinkMenuBinding>(inflater,
             R.layout.fragment_drink_menu, container, false)
+
+        binding.buttonJuice.setOnClickListener{view ->
+            view.findNavController().navigate(R.id.action_drinkMenuFragment_to_resultFragment)
+        }
+
+        binding.buttonSoda.setOnClickListener{view ->
+            view.findNavController().navigate(R.id.action_drinkMenuFragment_to_resultFragment)
+        }
+
+        binding.buttonCoffee.setOnClickListener{view ->
+            view.findNavController().navigate(R.id.action_drinkMenuFragment_to_resultFragment)
+        }
+
+        binding.buttonAlcohol.setOnClickListener{view ->
+            view.findNavController().navigate(R.id.action_drinkMenuFragment_to_resultFragment)
+        }
+
+
+
+
+
         return binding.root
     }
 
